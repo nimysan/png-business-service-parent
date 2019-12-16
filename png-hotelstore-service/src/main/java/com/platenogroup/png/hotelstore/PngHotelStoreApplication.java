c1dd01a0e12d8efd69964a1bec93e15230a9289c
Seann <yexw@btte.net>
2019-12-16 11:13:09 +0800

    增加门店项目

---
+++ png-hotelstore-service/src/main/java/com/platenogroup/png/hotelstore/PngHotelStoreApplication.java
@@ -0,0 +1,11 @@
+package com.platenogroup.png.hotelstore;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class PngHotelStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PngHotelStoreApplication.class, args);
+	}
+}
---
+++ png-hotelstore-service/src/test/java/com/platenogroup/png/hotelstore/web/controller/ReportControllerTest.java
@@ -0,0 +1,25 @@
+package com.platenogroup.png.hotelstore.web.controller;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class ReportControllerTest {
+
+	@Autowired
+	private WebTestClient webTestClient;
+
+	@Test
+	public void test() {
+		webTestClient.get().uri("/version").exchange().expectStatus().isOk();
+	}
+
+}
